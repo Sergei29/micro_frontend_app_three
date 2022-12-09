e79b1b0740bb8d2cc96941998caafd6c18f93689
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2022-12-09 16:34:21 +0200

    feat: imported components as micro-frontends

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config, options) => {
+    const { isServer } = options;
 
-module.exports = nextConfig
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "frontend_three",
+        filename: "static/chunks/remoteEntry_3.js",
+        remotes: {
+          frontend_one: `frontend_one@http://localhost:3000/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+          frontend_two: `frontend_two@http://localhost:3001/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry_2.js`,
+        },
+      })
+    );
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "@module-federation/nextjs-mf/src/include-defaults";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
